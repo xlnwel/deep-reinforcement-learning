51dd91805a58426a50db56d8384d2c5cc8b870ab
xlnwel <122134545@qq.com>
2018-08-11 05:16:58 +0800

    change hyperparameter search strategy

--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -4,24 +4,24 @@ import gym
 import numpy as np
 
 env = gym.make('Taxi-v2')
-alphas = [0.2, 0.3]
-epsilons = [0.2, 0.3]
-gammas = [0.4, 0.3]
+iterations = 10
+alphas = np.random.uniform(size=iterations)
+gammas = np.random.uniform(size=iterations)
+epsilons = np.random.uniform(size=iterations)
 records = {}
-for alpha in alphas:
-    for gamma in gammas:
-        for eps in epsilons:
-            for update_rule in ['Q_learning']:
-                print("Alpha: {}".format(alpha))
-                print("Gamma: {}".format(gamma))
-                print("Epsilon: {}".format(eps))
-                print("Update rule: {}".format(update_rule))
-                agent = Agent(alpha, gamma, eps, update_rule=update_rule)
-                avg_rewards, best_avg_reward = interact(env, agent, num_episodes=20000)
-                records[(alpha, gamma, eps)] = best_avg_reward
+for alpha, gamma, eps in zip(alphas, gammas, epsilons):
+    for update_rule in ['Q_learning']:
+        print("Alpha: {}".format(alpha))
+        print("Gamma: {}".format(gamma))
+        print("Epsilon: {}".format(eps))
+        print("Update rule: {}".format(update_rule))
+        agent = Agent(alpha, gamma, eps, update_rule=update_rule)
+        avg_rewards, best_avg_reward = interact(env, agent, num_episodes=20000)
+        records[(alpha, gamma, eps)] = best_avg_reward
 print('--------------------------------------------------')
-params = sorted(records.keys(), records.get())
+params = sorted(records.keys(), key=records.get)
 for alpha, gamma, eps in params:
+    print()
     print("Best alpha: {}".format(alpha))
     print("Best gamma: {}".format(gamma))
     print("Best epsilon: {}".format(eps))
