d2ba9029a8b8bec6125ad4f23ed759b74fb24ef2
xlnwel <122134545@qq.com>
2018-08-11 05:08:19 +0800

    modify print messages
    This reverts commit ac46f8cbc82a7ede67b39103314d8ba0b6dff3bb.

--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -7,8 +7,7 @@ env = gym.make('Taxi-v2')
 alphas = [0.2, 0.3]
 epsilons = [0.2, 0.3]
 gammas = [0.4, 0.3]
-best_val = 0
-best_param = None
+records = {}
 for alpha in alphas:
     for gamma in gammas:
         for eps in epsilons:
@@ -19,12 +18,11 @@ for alpha in alphas:
                 print("Update rule: {}".format(update_rule))
                 agent = Agent(alpha, gamma, eps, update_rule=update_rule)
                 avg_rewards, best_avg_reward = interact(env, agent, num_episodes=20000)
-                if best_avg_reward > best_val:
-                    best_param = (alpha, gamma, eps)
-                    best_val = best_avg_reward
-
-alpha, gamma, eps = best_param
-print("Best alpha: {}".format(alpha))
-print("Best gamma: {}".format(gamma))
-print("Best epsilon: {}".format(eps))
-print("Best Value: {}".format(best_val))
+                records[(alpha, gamma, eps)] = best_avg_reward
+print('--------------------------------------------------')
+params = sorted(records.keys(), records.get())
+for alpha, gamma, eps in params:
+    print("Best alpha: {}".format(alpha))
+    print("Best gamma: {}".format(gamma))
+    print("Best epsilon: {}".format(eps))
+    print("Best average value: {}".format(records[(alpha, gamma, eps)]))
