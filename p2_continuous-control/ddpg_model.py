6eb1b24f22fc9b408cad8cd46a257df6902777d7
xlnwel <122134545@qq.com>
2018-08-19 07:46:04 +0800

    Adding batch normalization to critic significantly improve performance

--- p2_continuous-control/ddpg_model.py
+++ p2_continuous-control/ddpg_model.py
@@ -29,7 +29,9 @@ class Critic(nn.Module):
     def __init__(self, state_size, action_size):
         super().__init__()
         self.fc1 = nn.Linear(state_size, 512)
+        self.bn1 = nn.BatchNorm1d(512)
         self.fc2 = nn.Linear(512 + action_size, 256)
+        self.bn2 = nn.BatchNorm1d(256)
         self.fc3 = nn.Linear(256, 1)
         self._reset_params()
 
@@ -43,7 +45,7 @@ class Critic(nn.Module):
     
     def forward(self, state, action):
         x = state
-        x = F.relu(self.fc1(x))
+        x = F.relu(self.bn1(self.fc1(x)))
         x = torch.cat((x, action), dim=1)
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.bn2(self.fc2(x)))
         return self.fc3(x)
