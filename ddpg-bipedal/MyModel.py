8b1093a25d7c3ddf521d8c22aeef47fdfd101bc8
xlnwel <122134545@qq.com>
2018-08-20 06:06:25 +0800

    Make the critic deeper

--- ddpg-bipedal/MyModel.py
+++ ddpg-bipedal/MyModel.py
@@ -34,11 +34,14 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     def __init__(self, state_size, action_size, param_file=None):
         super().__init__()
-        self.fc1 = nn.Linear(state_size, 512)
-        self.bn1 = nn.BatchNorm1d(512)
-        self.fc2 = nn.Linear(512 + action_size, 256)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.fc3 = nn.Linear(256, 1)
+        initial_hidden_units = 1024
+        self.fc1 = nn.Linear(state_size, initial_hidden_units)
+        self.bn1 = nn.BatchNorm1d(initial_hidden_units)
+        self.fc2 = nn.Linear(initial_hidden_units + action_size, initial_hidden_units // 2)
+        self.bn2 = nn.BatchNorm1d(initial_hidden_units // 2)
+        self.fc3 = nn.Linear(initial_hidden_units // 2, initial_hidden_units // 4)
+        self.bn3 = nn.BatchNorm1d(initial_hidden_units // 4)
+        self.fc4 = nn.Linear(initial_hidden_units // 4, 1)
         if param_file:
             self.load_params(param_file)
         else:
@@ -49,8 +52,10 @@ class Critic(nn.Module):
         nn.init.constant_(self.fc1.bias, 0)
         nn.init.kaiming_normal_(self.fc2.weight)
         nn.init.constant_(self.fc2.bias, 0)
-        nn.init.xavier_normal_(self.fc3.weight)
+        nn.init.kaiming_normal_(self.fc3.weight)
         nn.init.constant_(self.fc3.bias, 0)
+        nn.init.xavier_normal_(self.fc4.weight)
+        nn.init.constant_(self.fc4.bias, 0)
     
     def load_params(self, param_file):
         self.load_state_dict(torch.load(param_file))
@@ -59,4 +64,5 @@ class Critic(nn.Module):
         x = F.relu(self.bn1(self.fc1(state)))
         x = torch.cat((x, action), dim=1)
         x = F.relu(self.bn2(self.fc2(x)))
-        return self.fc3(x)
+        x = F.relu(self.bn3(self.fc3(x)))
+        return self.fc4(x)
